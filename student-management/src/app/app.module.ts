b8248b40c07b5e70795bb3b633d5127cb940586d
DamThiLanAnh <damthilananh8@hmail.com>
2025-08-14 08:43:45 +0700

    create component student

--- student-management/src/app/app.module.ts
+++ student-management/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentFormComponent } from './student/student-form/student-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    StudentFormComponent
   ],
   imports: [
     BrowserModule,
